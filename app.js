635632e155e41332a92bf9f254595751ea61a9d1
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-04-01 14:21:57 +0100

    day_1_first_commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// Data types & variables
+//var = myName = "Pav";
+//var myAge = 40;
+//var mySurname = "Gorny";
+//const DATA
+//console.log(myName + " " + mySurname);
+// conditionals
+var piNumber = 1234;
+
+if(piNumber === 1234) {
+  console.log("Correct pin number");
+} else {
+  console.console.log("Incorrect");
+}
+// loops
+var animals = ['cat', 'donkey', 'goose'];
+
+for (var i = 0; i < animals.length; i++) {
+  console.log(animals[i]);
+}
+// functions
+
+//var printNmae = function(){
+  //console.console.log('Pav');
+}
+
+printNmae()
+printNmae()
+printNmae()
+printNmae()
+// arrays
+
+var pubNames = ['cloisters', '4042']
+console.console.log(pubNames);
+// objects
+var person = {
+  name: "Pav",
+  telephone = 1234
+}
+console.console.log(person);
