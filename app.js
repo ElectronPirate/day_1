c22b9eaab63b86ea8f499af034a99b6a531ce82e
9338190 <a9338190@9338190s-MacBook-Pro.local>
2019-04-01 14:37:49 +0100

    day_1_first_commit-corrected a few typos

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
 // Data types & variables
-//var = myName = "Pav";
-//var myAge = 40;
-//var mySurname = "Gorny";
-//const DATA
-//console.log(myName + " " + mySurname);
-// conditionals
-var piNumber = 1234;
+var myName = "Pav";
+var myAge = 40;
+var mySurname = "Gorny";
 
-if(piNumber === 1234) {
+console.log(myName + " " + mySurname);
+// conditionals
+var pinNumber = 1234;
+
+if(pinNumber === 1234) {
   console.log("Correct pin number");
 } else {
   console.console.log("Incorrect");
 }
 // loops
-var animals = ['cat', 'donkey', 'goose'];
+ var animals = ['cat', 'donkey', 'goose'];
 
 for (var i = 0; i < animals.length; i++) {
   console.log(animals[i]);
 }
 // functions
 
-//var printNmae = function(){
-  //console.console.log('Pav');
+var printName = function(){
+ console.log('Pav');
 }
 
-printNmae()
-printNmae()
-printNmae()
-printNmae()
+printName()
 // arrays
 
-var pubNames = ['cloisters', '4042']
-console.console.log(pubNames);
+var pubNames = ['cloisters', '4042'];
+console.log(pubNames);
 // objects
 var person = {
   name: "Pav",
-  telephone = 1234
+  pinNumber: 1234,
 }
-console.console.log(person);
+console.log(person);
